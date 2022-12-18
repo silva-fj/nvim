d654b623e3e7142b0f2708f832bff6814084db85
Francisco Silva <franjs.francisco@gmail.com>
2022-12-18 16:05:36 +0100

    setting proper lsp mappings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,34 +3,34 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-    'tsserver',
-    'eslint',
-    'sumneko_lua',
-    'rust_analyzer',
+	"tsserver",
+	"eslint",
+	"sumneko_lua",
+	"rust_analyzer",
 })
 
-local cmp = require('cmp')
+local cmp = require("cmp")
 local cmp_select = { behavior = cmp.SelectBehavior.Insert }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-    ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-    ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.close(),
-    ["<Tab>"] = function(fallback)
-        if cmp.visible() then
-            cmp.select_next_item()
-        else
-            fallback()
-        end
-    end,
-    ["<CR>"] = cmp.mapping({
-        i = cmp.mapping.confirm({ select = true }),
-        c = cmp.mapping.confirm({
-            select = false,
-        }),
-    }),
+	["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+	["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+	["<C-d>"] = cmp.mapping.scroll_docs(-4),
+	["<C-f>"] = cmp.mapping.scroll_docs(4),
+	["<C-Space>"] = cmp.mapping.complete(),
+	["<C-e>"] = cmp.mapping.close(),
+	["<Tab>"] = function(fallback)
+		if cmp.visible() then
+			cmp.select_next_item()
+		else
+			fallback()
+		end
+	end,
+	["<CR>"] = cmp.mapping({
+		i = cmp.mapping.confirm({ select = true }),
+		c = cmp.mapping.confirm({
+			select = false,
+		}),
+	}),
 })
 
 -- disable completion with tab
@@ -39,33 +39,66 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
 -- cmp_mappings['<S-Tab>'] = nil
 
 lsp.setup_nvim_cmp({
-    mapping = cmp_mappings
+	mapping = cmp_mappings,
 })
 
 vim.diagnostic.config({
-    virtual_text = true,
+	virtual_text = true,
 })
 
 lsp.on_attach(function(client, bufnr)
-    print("LSP started.")
-    local opts = { buffer = bufnr, remap = false }
+	print("LSP started.")
 
-    if client.name == "eslint" then
-        vim.cmd.LspStop('eslint')
-        return
-    end
+	if client.name == "eslint" then
+		vim.cmd.LspStop("eslint")
+		return
+	end
 
-    local function buf_set_keymap(...)
-        vim.api.nvim_buf_set_keymap(bufnr, ...)
-    end
+	-- Create a command `:Format` local to the LSP buffer
+	vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+		if vim.lsp.buf.format then
+			vim.lsp.buf.format()
+		elseif vim.lsp.buf.formatting then
+			vim.lsp.buf.formatting()
+		end
+	end, { desc = "Format current buffer with LSP" })
 
-    local opts = { noremap = true, silent = true }
-    buf_set_keymap("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-    buf_set_keymap("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-    buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-    buf_set_keymap("n", "<leader>a", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-    buf_set_keymap("n", "<space>d", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-    buf_set_keymap("n", "ff", "<cmd>lua vim.lsp.buf.format()<CR>", opts)
+	local nmap = function(keys, func, desc)
+		if desc then
+			desc = "LSP: " .. desc
+		end
+
+		vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+	end
+
+	nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+	nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+	nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+	nmap("gr", function ()
+	   require("telescope.builtin").lsp_references({ layout_strategy = 'vertical' })
+	end, "[G]oto [R]eferences")
+	nmap("gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+	nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+	nmap("<leader>ds", function ()
+	   require("telescope.builtin").lsp_document_symbols({ layout_strategy = 'vertical' })
+	end, "[D]ocument [S]ymbols")
+	nmap("<leader>ws", function ()
+	   require("telescope.builtin").lsp_dynamic_workspace_symbols({ layout_strategy = 'vertical' })
+	end, "[W]orkspace [S]ymbols")
+	nmap("<space>ld", vim.diagnostic.open_float, "[L]ine [D]iagnostic")
+	nmap("ff", "<cmd>Format<CR>", "[F]ormat [F]ile")
+
+	-- See `:help K` for why this keymap
+	nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+	nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+
+	-- Lesser used LSP functionality
+	nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+	nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
+	nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
+	nmap("<leader>wl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, "[W]orkspace [L]ist Folders")
 end)
 
 lsp.nvim_workspace()
