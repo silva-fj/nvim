83d70fdd5a225da0593504794c4ea13b875a73b8
Francisco Silva <franjs.francisco@gmail.com>
2022-12-19 18:46:39 +0100

    changing mappings for comments

--- after/plugin/comment.lua
+++ after/plugin/comment.lua
@@ -1,3 +1,18 @@
-require('Comment').setup {
-  pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-}
+require("Comment").setup({
+	pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+	toggler = {
+		---Line-comment toggle keymap
+		line = "<leader>cc",
+		---Block-comment toggle keymap
+		block = "<leader>cb",
+	},
+	opleader = {
+		---Line-comment keymap
+		line = "<leader>cc",
+		---Block-comment keymap
+		block = "<leader>cb",
+	},
+	mappings = {
+		extra = false,
+	},
+})
