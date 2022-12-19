9b6f07d58d82af1b9f3cbd9058f7e955398960cc
Francisco Silva <franjs.francisco@gmail.com>
2022-12-19 16:25:40 +0100

    formatting on save

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -1,15 +1,28 @@
 local null_ls = require("null-ls")
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
-    sources = {
-        null_ls.builtins.formatting.prettier.with({
-            prefer_local = "node_modules/.bin",
-        }),
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.write_good,
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.code_actions.eslint_d,
-        null_ls.builtins.code_actions.gitsigns,
-        null_ls.builtins.code_actions.xo,
-    },
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
+	sources = {
+		null_ls.builtins.formatting.prettier.with({
+			prefer_local = "node_modules/.bin",
+		}),
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.diagnostics.write_good,
+		null_ls.builtins.diagnostics.eslint,
+		null_ls.builtins.code_actions.eslint_d,
+		null_ls.builtins.code_actions.gitsigns,
+		null_ls.builtins.code_actions.xo,
+	},
 })
