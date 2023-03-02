b21e510cf937ae5e98b57072a591c7cd7c9c6d7c
Francisco Silva <franjs.francisco@gmail.com>
2023-03-02 12:09:28 +0100

    updating colorschema and formatting code

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,116 +1,115 @@
 return {
-    -- Colorschemes
-    {
-        "ellisonleao/gruvbox.nvim",
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
-        config = function()
-            -- load the colorscheme here
-            vim.cmd.colorscheme("gruvbox")
-        end,
-    },
-    "mhartington/oceanic-next",
-    "Mofiqul/vscode.nvim",
-    "ellisonleao/gruvbox.nvim",
-    "mhartington/oceanic-next",
-    "folke/tokyonight.nvim",
-    "RRethy/vim-illuminate",
-    {
-        -- Useful status updates for LSP
-        "j-hui/fidget.nvim",
-        config = function()
-            require("fidget").setup()
-        end,
-    },
-    {
-        "folke/trouble.nvim",
-        dependencies = "nvim-tree/nvim-web-devicons",
-        config = function()
-            require("trouble").setup()
-            vim.api.nvim_set_keymap(
-                "n",
-                "<leader>ge",
-                "<cmd>Trouble document_diagnostics<cr>",
-                { silent = true, noremap = true }
-            )
-            vim.api.nvim_set_keymap(
-                "n",
-                "<leader>gE",
-                "<cmd>Trouble workspace_diagnostics<cr>",
-                { silent = true, noremap = true }
-            )
-            vim.api.nvim_set_keymap("n", "gr", "<cmd>Trouble lsp_references<cr>", { silent = true, noremap = true })
-        end,
-    },
-    "ray-x/lsp_signature.nvim",
+	-- Colorschemes
+	"ellisonleao/gruvbox.nvim",
+	"mhartington/oceanic-next",
+	{
+		"Mofiqul/vscode.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			vim.cmd.colorscheme("vscode")
+		end,
+	},
+	"ellisonleao/gruvbox.nvim",
+	"mhartington/oceanic-next",
+	"folke/tokyonight.nvim",
+	"RRethy/vim-illuminate",
+	{
+		-- Useful status updates for LSP
+		"j-hui/fidget.nvim",
+		config = function()
+			require("fidget").setup()
+		end,
+	},
+	{
+		"folke/trouble.nvim",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		config = function()
+			require("trouble").setup()
+			vim.api.nvim_set_keymap(
+				"n",
+				"<leader>ge",
+				"<cmd>Trouble document_diagnostics<cr>",
+				{ silent = true, noremap = true }
+			)
+			vim.api.nvim_set_keymap(
+				"n",
+				"<leader>gE",
+				"<cmd>Trouble workspace_diagnostics<cr>",
+				{ silent = true, noremap = true }
+			)
+			vim.api.nvim_set_keymap("n", "gr", "<cmd>Trouble lsp_references<cr>", { silent = true, noremap = true })
+		end,
+	},
+	"ray-x/lsp_signature.nvim",
 
-    -- Git
-    "tpope/vim-rhubarb",
-    {
-        "kdheepak/lazygit.nvim",
-        config = function()
-            vim.api.nvim_set_keymap("n", "<Leader>lg", ":LazyGit<CR>", { noremap = true, silent = true })
-        end,
-    },
+	-- Git
+	"tpope/vim-rhubarb",
+	{
+		"kdheepak/lazygit.nvim",
+		config = function()
+			vim.api.nvim_set_keymap("n", "<Leader>lg", ":LazyGit<CR>", { noremap = true, silent = true })
+		end,
+	},
 
-    {
-        "folke/todo-comments.nvim",
-        dependencies = "nvim-lua/plenary.nvim",
-        config = function()
-            require("todo-comments").setup()
-        end,
-    },
+	{
+		"folke/todo-comments.nvim",
+		dependencies = "nvim-lua/plenary.nvim",
+		config = function()
+			require("todo-comments").setup()
+		end,
+	},
 
-    "christoomey/vim-tmux-navigator",
-    {
-        "nvim-tree/nvim-web-devicons",
-        config = function()
-            require("nvim-web-devicons").setup({ default = true })
-        end,
-    },
-    {
-        "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
-        config = function()
-            require("lualine").setup()
-        end,
-    },
-    { "romgrk/barbar.nvim", dependencies = "nvim-tree/nvim-web-devicons" },
-    {
-        "goolord/alpha-nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            require("alpha").setup(require("alpha.themes.startify").config)
-        end,
-    },
-    {
-        "windwp/nvim-autopairs",
-        config = function()
-            require("nvim-autopairs").setup()
-        end,
-    },
-    "tpope/vim-surround",
-    {
-        "windwp/nvim-ts-autotag",
-        config = function()
-            require("nvim-ts-autotag").setup()
-        end,
-    },
-    {
-        "kosayoda/nvim-lightbulb",
-        config = function()
-            require("nvim-lightbulb").setup({ autocmd = { enabled = true } })
-        end,
-    },
-    "folke/lsp-colors.nvim",
-    {
-        "windwp/nvim-spectre",
-        config = function()
-            require("spectre").setup()
-            local keymap = vim.keymap.set
+	"christoomey/vim-tmux-navigator",
+	{
+		"nvim-tree/nvim-web-devicons",
+		config = function()
+			require("nvim-web-devicons").setup({ default = true })
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
+		config = function()
+			require("lualine").setup()
+		end,
+	},
+	{ "romgrk/barbar.nvim", dependencies = "nvim-tree/nvim-web-devicons" },
+	{
+		"goolord/alpha-nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("alpha").setup(require("alpha.themes.startify").config)
+		end,
+	},
+	{
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup()
+		end,
+	},
+	"tpope/vim-surround",
+	{
+		"windwp/nvim-ts-autotag",
+		config = function()
+			require("nvim-ts-autotag").setup()
+		end,
+	},
+	{
+		"kosayoda/nvim-lightbulb",
+		config = function()
+			require("nvim-lightbulb").setup({ autocmd = { enabled = true } })
+		end,
+	},
+	"folke/lsp-colors.nvim",
+	{
+		"windwp/nvim-spectre",
+		config = function()
+			require("spectre").setup()
+			local keymap = vim.keymap.set
 
-            -- Code action
-            keymap({ "n" }, "<leader>S", "<cmd>lua require('spectre').open()<CR>")
-        end,
-    },
+			-- Code action
+			keymap({ "n" }, "<leader>S", "<cmd>lua require('spectre').open()<CR>")
+		end,
+	},
 }
--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -1,69 +1,69 @@
 return {
-    "glepnir/lspsaga.nvim",
-    branch = "main",
-    dependencies = {
-        { "nvim-tree/nvim-web-devicons" },
-        --Please make sure you install markdown and markdown_inline parser
-        { "nvim-treesitter/nvim-treesitter" },
-    },
-    config = function()
-        require("lspsaga").setup({
-            rename = {
-                quit = "<Esc>",
-                exec = "<CR>",
-                mark = "x",
-                confirm = "<CR>",
-                in_select = true,
-            },
-            ui = {
-                -- This option only works in Neovim 0.9
-                title = true,
-                -- Border type can be single, double, rounded, solid, shadow.
-                border = "rounded",
-                winblend = 0,
-                expand = "",
-                collapse = "",
-                code_action = "💡",
-                incoming = " ",
-                outgoing = " ",
-                hover = " ",
-                kind = {},
-            },
-            symbol_in_winbar = {
-                enable = false,
-            },
-        })
+	"glepnir/lspsaga.nvim",
+	branch = "main",
+	dependencies = {
+		{ "nvim-tree/nvim-web-devicons" },
+		--Please make sure you install markdown and markdown_inline parser
+		{ "nvim-treesitter/nvim-treesitter" },
+	},
+	config = function()
+		require("lspsaga").setup({
+			rename = {
+				quit = "<Esc>",
+				exec = "<CR>",
+				mark = "x",
+				confirm = "<CR>",
+				in_select = true,
+			},
+			ui = {
+				-- This option only works in Neovim 0.9
+				title = true,
+				-- Border type can be single, double, rounded, solid, shadow.
+				border = "rounded",
+				winblend = 0,
+				expand = "",
+				collapse = "",
+				code_action = "💡",
+				incoming = " ",
+				outgoing = " ",
+				hover = " ",
+				kind = {},
+			},
+			symbol_in_winbar = {
+				enable = false,
+			},
+		})
 
-        local keymap = vim.keymap.set
+		local keymap = vim.keymap.set
 
-        -- Code action
-        keymap({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>")
+		-- Code action
+		keymap({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>")
 
-        -- Rename all occurrences of the hovered word for the entire file
-        keymap("n", "rn", "<cmd>Lspsaga rename<CR>")
+		-- Rename all occurrences of the hovered word for the entire file
+		keymap("n", "rn", "<cmd>Lspsaga rename<CR>")
 
-        -- Show line diagnostics
-        -- You can pass argument ++unfocus to
-        -- unfocus the show_line_diagnostics floating window
-        keymap("n", "<leader>dl", "<cmd>Lspsaga show_line_diagnostics<CR>")
+		-- Show line diagnostics
+		-- You can pass argument ++unfocus to
+		-- unfocus the show_line_diagnostics floating window
+		keymap("n", "<leader>dl", "<cmd>Lspsaga show_line_diagnostics<CR>")
 
-        -- Show buffer diagnostics
-        keymap("n", "<leader>db", "<cmd>Lspsaga show_buf_diagnostics<CR>")
+		-- Show buffer diagnostics
+		keymap("n", "<leader>db", "<cmd>Lspsaga show_buf_diagnostics<CR>")
 
-        -- Diagnostic jump
-        -- You can use <C-o> to jump back to your previous location
-        keymap("n", "[g", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
-        keymap("n", "]g", "<cmd>Lspsaga diagnostic_jump_next<CR>")
+		-- Diagnostic jump
+		-- You can use <C-o> to jump back to your previous location
+		keymap("n", "[g", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
+		keymap("n", "]g", "<cmd>Lspsaga diagnostic_jump_next<CR>")
 
-        -- Hover Doc
-        -- If there is no hover doc,
-        -- there will be a notification stating that
-        -- there is no information available.
-        -- To disable it just use ":Lspsaga hover_doc ++quiet"
-        -- Pressing the key twice will enter the hover window
-        keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>")
+		-- Hover Doc
+		-- If there is no hover doc,
+		-- there will be a notification stating that
+		-- there is no information available.
+		-- To disable it just use ":Lspsaga hover_doc ++quiet"
+		-- Pressing the key twice will enter the hover window
+		keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>")
 
-        -- Floating terminal
-        keymap({ "n", "t" }, "<leader>t", "<cmd>Lspsaga term_toggle<CR>")
-    end,
+		-- Floating terminal
+		keymap({ "n", "t" }, "<leader>t", "<cmd>Lspsaga term_toggle<CR>")
+	end,
 }
