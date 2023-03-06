3e8d14e9cbb10440c9ea3da502c61e03eb74f9ea
Francisco Silva <franjs.francisco@gmail.com>
2023-03-06 09:18:00 +0100

    installing refactoring plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -121,4 +121,20 @@ return {
 			keymap({ "n" }, "<leader>S", "<cmd>lua require('spectre').open()<CR>")
 		end,
 	},
+	{
+		"ThePrimeagen/refactoring.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("refactoring").setup()
+			vim.api.nvim_set_keymap(
+				"v",
+				"<leader>rr",
+				":lua require('refactoring').select_refactor()<CR>",
+				{ noremap = true, silent = true, expr = false }
+			)
+		end,
+	},
 }
