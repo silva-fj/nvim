156339213da27248bb57f6d1e55281201619df2e
Francisco Silva <franjs.francisco@gmail.com>
2023-03-19 11:13:40 +0100

    adding current file key map for spectre

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -144,10 +144,12 @@ return {
 		"windwp/nvim-spectre",
 		config = function()
 			require("spectre").setup()
-			local keymap = vim.keymap.set
 
 			-- Code action
-			keymap({ "n" }, "<leader>S", "<cmd>lua require('spectre').open()<CR>")
+			vim.keymap.set({ "n" }, "<leader>S", "<cmd>lua require('spectre').open()<CR>")
+			vim.keymap.set("n", "<leader>SS", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+				desc = "Search on current file",
+			})
 		end,
 	},
 	{
