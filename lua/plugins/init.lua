5f4c48f8ddc6d22cdceb2692047ed5c5cdd65798
Francisco Silva <franjs.francisco@gmail.com>
2023-03-23 19:57:06 +0100

    making gruvbox default theme

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,14 @@
 return {
     -- Colorschemes
     -- gruvbox
-    { "ellisonleao/gruvbox.nvim" },
+    {
+        "ellisonleao/gruvbox.nvim",
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            vim.cmd.colorscheme("gruvbox")
+        end,
+    },
     {
         "Mofiqul/vscode.nvim",
     },
@@ -20,11 +27,6 @@ return {
     -- codedark
     {
         "tomasiser/vim-code-dark",
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
-        config = function()
-            vim.cmd.colorscheme("codedark")
-        end,
     },
     -- nightfly
     {
