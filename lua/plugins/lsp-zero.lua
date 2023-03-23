a87f9756cebaacea498968b2cf0197f2b1721f65
Francisco Silva <franjs.francisco@gmail.com>
2023-03-23 09:29:12 +0100

    fixing copilot integration

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -62,13 +62,6 @@ return {
                 ["<C-f>"] = cmp.mapping.scroll_docs(4),
                 ["<C-Space>"] = cmp.mapping.complete(),
                 ["<C-e>"] = cmp.mapping.close(),
-                ["<Tab>"] = function(fallback)
-                if cmp.visible() then
-                    cmp.select_next_item()
-                else
-                    fallback()
-                end
-            end,
                 ["<CR>"] = cmp.mapping({
                 i = cmp.mapping.confirm({ select = true }),
                 c = cmp.mapping.confirm({
@@ -78,6 +71,7 @@ return {
         })
 
         lsp.setup_nvim_cmp({
+            -- preselect = cmp.PreselectMode.None,
             mapping = cmp_mappings,
             formatting = {
                 format = function(entry, vim_item)
