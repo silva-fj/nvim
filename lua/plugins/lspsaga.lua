9e43c16c2d13841b36e607f6e2bc546701495845
Francisco Silva <franjs.francisco@gmail.com>
2023-07-20 09:19:06 +0200

    updating lspsaga

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -1,80 +1,70 @@
 return {
-	"glepnir/lspsaga.nvim",
-	branch = "main",
-	dependencies = {
-		{ "nvim-tree/nvim-web-devicons" },
-		--Please make sure you install markdown and markdown_inline parser
-		{ "nvim-treesitter/nvim-treesitter" },
-	},
-	config = function()
-		require("lspsaga").setup({
-			rename = {
-				quit = "<Esc>",
-				exec = "<CR>",
-				mark = "x",
-				confirm = "<CR>",
-				in_select = true,
-			},
-			ui = {
-				-- This option only works in Neovim 0.9
-				title = true,
-				-- Border type can be single, double, rounded, solid, shadow.
-				border = "rounded",
-				winblend = 0,
-				expand = "",
-				collapse = "",
-				code_action = "💡",
-				incoming = " ",
-				outgoing = " ",
-				hover = " ",
-				kind = {},
-			},
-			symbol_in_winbar = {
-				enable = false,
-			},
-			code_action = {
-				num_shortcut = true,
-				show_server_name = true,
-				extend_gitsigns = true,
-				keys = {
-					-- string | table type
-					quit = "q",
-					exec = "<CR>",
-				},
-			},
-			lightbulb = {
-				enable = false,
-				enable_in_insert = false,
-			},
-		})
+    "nvimdev/lspsaga.nvim",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        require("lspsaga").setup({
+            rename = {
+                in_select = true,
+                auto_save = false,
+                project_max_width = 0.5,
+                project_max_height = 0.5,
+                keys = {
+                    quit = "<Esc>",
+                    exec = "<CR>",
+                    select = "x",
+                },
+            },
+            ui = {
+                border = "single",
+                devicon = true,
+                title = true,
+                expand = "⊞",
+                collapse = "⊟",
+                code_action = "💡",
+                actionfix = " ",
+                lines = { "┗", "┣", "┃", "━", "┏" },
+                kind = nil,
+                imp_sign = "󰳛 ",
+            },
+            symbol_in_winbar = {
+                enable = false,
+            },
+            lightbulb = {
+                enable = false,
+                enable_in_insert = false,
+            },
+        })
 
-		local keymap = vim.keymap.set
+        local keymap = vim.keymap.set
 
-		-- Code action
-		keymap({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>")
+        -- Code action
+        keymap({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>")
 
-		-- Rename all occurrences of the hovered word for the entire file
-		keymap("n", "rn", "<cmd>Lspsaga rename<CR>")
+        -- Rename all occurrences of the hovered word for the entire file
+        keymap("n", "rn", "<cmd>Lspsaga rename<CR>")
 
-		-- Show line diagnostics
-		-- You can pass argument ++unfocus to
-		-- unfocus the show_line_diagnostics floating window
-		keymap("n", "<leader>dl", "<cmd>Lspsaga show_line_diagnostics<CR>")
+        -- Show line diagnostics
+        -- You can pass argument ++unfocus to
+        -- unfocus the show_line_diagnostics floating window
+        keymap("n", "<leader>dl", "<cmd>Lspsaga show_line_diagnostics<CR>")
 
-		-- Diagnostic jump
-		-- You can use <C-o> to jump back to your previous location
-		keymap("n", "[g", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
-		keymap("n", "]g", "<cmd>Lspsaga diagnostic_jump_next<CR>")
+        -- Diagnostic jump
+        -- You can use <C-o> to jump back to your previous location
+        keymap("n", "[g", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
+        keymap("n", "]g", "<cmd>Lspsaga diagnostic_jump_next<CR>")
 
-		-- Hover Doc
-		-- If there is no hover doc,
-		-- there will be a notification stating that
-		-- there is no information available.
-		-- To disable it just use ":Lspsaga hover_doc ++quiet"
-		-- Pressing the key twice will enter the hover window
-		-- keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>")
+        -- Hover Doc
+        -- If there is no hover doc,
+        -- there will be a notification stating that
+        -- there is no information available.
+        -- To disable it just use ":Lspsaga hover_doc ++quiet"
+        -- Pressing the key twice will enter the hover window
+        -- keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>")
 
-		-- Floating terminal
-		keymap({ "n", "t" }, "<leader>t", "<cmd>Lspsaga term_toggle<CR>")
-	end,
+        -- Floating terminal
+        keymap({ "n", "t" }, "<leader>t", "<cmd>Lspsaga term_toggle<CR>")
+    end,
 }
