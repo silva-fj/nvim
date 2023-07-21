a1b6fb0ca55b7d9c6554b547a28730bf89c33248
Francisco Silva <franjs.francisco@gmail.com>
2023-07-21 09:40:34 +0200

    updating telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	version = "0.1.1",
+	version = "0.1.2",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
