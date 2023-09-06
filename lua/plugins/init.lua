0c834d732ecf1043a47b7fc5f520fb10ad60bd4e
Francisco Silva <franjs.francisco@gmail.com>
2023-09-06 17:45:40 +0200

    fixing nvim-ts-autotag config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -120,7 +120,18 @@ return {
         "windwp/nvim-ts-autotag",
         config = function()
             require("nvim-ts-autotag").setup({
+                enable = true,
+                enable_rename = true,
+                enable_close = true,
                 enable_close_on_slash = false,
+                filetypes = {
+                    'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue', 'tsx',
+                    'jsx', 'rescript',
+                    'xml',
+                    'php',
+                    'markdown',
+                    'astro', 'glimmer', 'handlebars', 'hbs'
+                }
             })
         end,
     },
