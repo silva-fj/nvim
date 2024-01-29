ac6fe485581f2f902ff6cf5cd04204ddfe92b3e0
Francisco Silva <franjs.francisco@gmail.com>
2024-01-29 21:08:20 +0100

    updating fidget

--- lua/plugins/fidget.lua
+++ lua/plugins/fidget.lua
@@ -1,6 +1,18 @@
 -- Useful status updates for LSP
 return {
     "j-hui/fidget.nvim",
-    config = true,
-    tag = "v1.1.0",
+    opts = {
+        integration = {
+            ["nvim-tree"] = {
+                enable = true,
+            },
+        },
+        -- Options related to logging
+        logger = {
+            level = vim.log.levels.WARN, -- Minimum logging level
+            float_precision = 0.01,      -- Limit the number of decimals displayed for floats
+            path =                       -- Where Fidget writes its logs to
+                string.format("%s/fidget.nvim.log", vim.fn.stdpath("cache")),
+        },
+    },
 }
