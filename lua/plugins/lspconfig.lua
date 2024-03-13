ed966b7c7d2bacd4c2d60175fbf6c49053442b26
Francisco Silva <franjs.francisco@gmail.com>
2024-03-13 11:42:08 +0100

    don't setup rust-analyzer automatically

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -81,14 +81,17 @@ return {
                 "dockerls",
                 "cssls",
                 "cssmodules_ls",
+                "rust_analyzer",
             },
             automatic_installation = true,
             handlers = {
                 function(server_name)
-                    local capabilities = require('cmp_nvim_lsp').default_capabilities()
-                    require("lspconfig")[server_name].setup {
-                        capabilities = capabilities
-                    }
+                    if server_name ~= "rust_analyzer" then
+                        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+                        require("lspconfig")[server_name].setup {
+                            capabilities = capabilities
+                        }
+                    end
                 end,
             },
         })
