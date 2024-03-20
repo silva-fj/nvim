a1d56bc769f5cff045a708086362faceca4b8902
Francisco Silva <franjs.francisco@gmail.com>
2024-03-20 09:27:30 +0100

    adding new dependency to neotest

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -4,6 +4,7 @@ return {
         "nvim-lua/plenary.nvim",
         "antoinemadec/FixCursorHold.nvim",
         "nvim-treesitter/nvim-treesitter",
+        "nvim-neotest/nvim-nio",
 
         -- Adapters
         'nvim-neotest/neotest-jest',
