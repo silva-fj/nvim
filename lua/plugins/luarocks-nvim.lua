3965c55e655f341b3906de3cf2190e17a68d64ef
Francisco Silva <franjs.francisco@gmail.com>
2024-03-25 16:41:02 +0100

    disabling luarocks for now

--- lua/plugins/luarocks-nvim.lua
+++ lua/plugins/luarocks-nvim.lua
@@ -1,8 +1,8 @@
 return {
-    "vhyrro/luarocks.nvim",
-    priority = 1000,
-    opts = {
-        rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
-    },
-    config = true,
+    -- "vhyrro/luarocks.nvim",
+    -- priority = 1000,
+    -- opts = {
+    --     rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
+    -- },
+    -- config = true,
 }
--- lua/plugins/rest-nvim.lua
+++ lua/plugins/rest-nvim.lua
@@ -1,15 +1,15 @@
 return {
-    "rest-nvim/rest.nvim",
-    ft = "http",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "vhyrro/luarocks.nvim",
-    },
-    config = function()
-        require("rest-nvim").setup()
-
-        local opts = { noremap = true, silent = true }
-        vim.api.nvim_set_keymap("n", "<leader>rr", "<cmd>Rest run<cr>", opts)
-        vim.api.nvim_set_keymap("n", "<leader>rl", "<cmd>Rest run last<cr>", opts)
-    end
+    -- "rest-nvim/rest.nvim",
+    -- ft = "http",
+    -- dependencies = {
+    --     "nvim-lua/plenary.nvim",
+    --     "vhyrro/luarocks.nvim",
+    -- },
+    -- config = function()
+    --     require("rest-nvim").setup()
+    --
+    --     local opts = { noremap = true, silent = true }
+    --     vim.api.nvim_set_keymap("n", "<leader>rr", "<cmd>Rest run<cr>", opts)
+    --     vim.api.nvim_set_keymap("n", "<leader>rl", "<cmd>Rest run last<cr>", opts)
+    -- end
 }
