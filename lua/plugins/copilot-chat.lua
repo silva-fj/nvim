a856710204ceecc3c01270d20318cec86d9b3a3f
Francisco Silva <franjs.francisco@gmail.com>
2024-03-26 08:51:17 +0100

    updating copilot-chat mappings

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -9,7 +9,38 @@ return {
         },
         opts = {
             debug = true, -- Enable debugging
-            -- See Configuration section for rest
+            -- default mappings
+            mappings = {
+                complete = {
+                    detail = 'Use @<Tab> or /<Tab> for options.',
+                    insert = '<Tab>',
+                },
+                close = {
+                    normal = 'q',
+                    insert = '<C-c>'
+                },
+                reset = {
+                    normal = '<C-l>',
+                    insert = '<C-r>'
+                },
+                submit_prompt = {
+                    normal = '<CR>',
+                    insert = '<C-m>'
+                },
+                accept_diff = {
+                    normal = '<C-y>',
+                    insert = '<C-y>'
+                },
+                show_diff = {
+                    normal = 'gd'
+                },
+                show_system_prompt = {
+                    normal = 'gp'
+                },
+                show_user_selection = {
+                    normal = 'gs'
+                },
+            },
         },
         keys = {
             {
