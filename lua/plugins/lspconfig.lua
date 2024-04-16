bc5c36de35fcc2411fcdd9f6b9ae63800e485270
Francisco Silva <franjs.francisco@gmail.com>
2024-04-16 15:04:40 +0200

    updating tsserver settings

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -127,10 +127,33 @@ return {
         end
 
         lspconfig.tsserver.setup({
-            on_attach = function(client)
-                client.resolved_capabilities.document_formatting = false
-                client.resolved_capabilities.document_range_formatting = false
-            end
+            settings = {
+                typescript = {
+                    inlayHints = {
+                        -- taken from https://github.com/typescript-language-server/typescript-language-server#workspacedidchangeconfiguration
+                        includeInlayEnumMemberValueHints = true,
+                        includeInlayFunctionLikeReturnTypeHints = true,
+                        includeInlayFunctionParameterTypeHints = true,
+                        includeInlayParameterNameHints = 'all',
+                        includeInlayParameterNameHintsWhenArgumentMatchesName = true, -- false
+                        includeInlayPropertyDeclarationTypeHints = true,
+                        includeInlayVariableTypeHints = true,
+                        includeInlayVariableTypeHintsWhenTypeMatchesName = true -- false
+                    }
+                },
+                javascript = {
+                    inlayHints = {
+                        includeInlayEnumMemberValueHints = true,
+                        includeInlayFunctionLikeReturnTypeHints = true,
+                        includeInlayFunctionParameterTypeHints = true,
+                        includeInlayParameterNameHints = 'all',
+                        includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                        includeInlayPropertyDeclarationTypeHints = true,
+                        includeInlayVariableTypeHints = true,
+                        includeInlayVariableTypeHintsWhenTypeMatchesName = true
+                    }
+                },
+            },
         })
 
         lspconfig.html.setup({
