e10e3e6e8371a0d90a83760dcd955c07dbe6ce25
Francisco Silva <franjs.francisco@gmail.com>
2024-04-20 14:38:49 +0200

    updating settings

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,14 +1,14 @@
 local set = vim.o
 
 set.autowrite = true
-set.scrolloff = 5
+set.scrolloff = 10
 set.si = true
 set.expandtab = true
 set.shiftwidth = 4
 set.tabstop = 4
 set.smartindent = true
 set.number = true
-set.relativenumber = true
+set.relativenumber = false
 set.splitbelow = true
 set.splitright = true
 set.shell = "/bin/zsh"
@@ -17,10 +17,25 @@ set.backup = false
 set.writebackup = false
 set.signcolumn = "yes"
 set.cmdheight = 1
-set.updatetime = 100
+set.mouse = "a"
+set.showmode = false
+set.undofile = true
 
-vim.opt.clipboard = { "unnamed", "unnamedplus" }
+vim.opt.breakindent = true
+vim.opt.clipboard = "unnamedplus"
 vim.opt.wildignore = { "*/temp/*", "*.so", "*.zip", "*/node_modules/*", "*/vendor/bundle/*", "*/vendor/*" }
+vim.opt.hlsearch = true
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+-- Decrease update time
+vim.opt.updatetime = 250
+-- Decrease mapped sequence wait time
+vim.opt.timeoutlen = 300
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+-- Show which line your cursor is on
+vim.opt.cursorline = true
 
 -- disable netrw
 -- vim.g.loaded_netrw = 1
@@ -28,8 +43,3 @@ vim.opt.wildignore = { "*/temp/*", "*.so", "*.zip", "*/node_modules/*", "*/vendo
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
-
--- Set highlight on search
-vim.o.hlsearch = false
--- Save undo history
-vim.o.undofile = false
