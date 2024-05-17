ce63248a9b882c5051b17a880bf46d3db4d940d6
Francisco Silva <franjs.francisco@gmail.com>
2024-05-17 09:25:07 +0200

    putting back bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,16 +1,15 @@
 return {
-    -- "akinsho/bufferline.nvim",
-    -- version = "v4.*",
-    -- dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
-    -- config = function()
-    --     require("bufferline").setup({
-    --         options = {
-    --             diagnostics = "nvim_lsp",
-    --             diagnostics_indicator = function(count, level)
-    --                 local icon = level:match("error") and " " or " "
-    --                 return " " .. icon .. count
-    --             end,
-    --         },
-    --     })
-    -- end,
+    "akinsho/bufferline.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
+    config = function()
+        require("bufferline").setup({
+            options = {
+                diagnostics = "nvim_lsp",
+                diagnostics_indicator = function(count, level)
+                    local icon = level:match("error") and " " or " "
+                    return " " .. icon .. count
+                end,
+            },
+        })
+    end,
 }
