18b1609ceef269ad268e297f9021407cee45e91e
Francisco Silva <franjs.francisco@gmail.com>
2024-05-26 13:43:08 +0200

    updating nvim-ts-autotag config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -29,19 +29,12 @@ return {
         "windwp/nvim-ts-autotag",
         config = function()
             require("nvim-ts-autotag").setup({
-                enable = true,
-                enable_rename = true,
-                enable_close = true,
-                enable_close_on_slash = false,
-                filetypes = {
-                    'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue', 'tsx',
-                    'jsx', 'rescript',
-                    'xml',
-                    'php',
-                    'markdown',
-                    'astro', 'glimmer', 'handlebars', 'hbs',
-                    'rust'
-                }
+                opts = {
+                    -- Defaults
+                    enable_close = true,          -- Auto close tags
+                    enable_rename = true,         -- Auto rename pairs of tags
+                    enable_close_on_slash = false -- Auto close on trailing </
+                },
             })
         end,
     }
