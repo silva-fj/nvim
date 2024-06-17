d651cc73b621db0317f51087d9d4a0683c36b05a
Francisco Silva <franjs.francisco@gmail.com>
2024-06-17 10:52:18 +0200

    updating fugitive's config

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,9 +2,8 @@ return {
     "tpope/vim-fugitive",
     config = function()
         vim.api.nvim_set_keymap("n", "<Leader>gs", ":Git<CR>", { noremap = true })
-        vim.api.nvim_set_keymap("n", "<Leader>gd", ":Gdiffsplit<CR>", { noremap = true })
+        vim.api.nvim_set_keymap("n", "<Leader>gd", ":Gvdiffsplit<CR>", { noremap = true })
+        vim.api.nvim_set_keymap("n", "<Leader>gD", ":Gvdiffsplit!<CR>", { noremap = true })
         vim.api.nvim_set_keymap("n", "<Leader>gb", ":GBrowse<CR>", { noremap = true })
-
-        vim.cmd([[ autocmd BufReadPost fugitive://* set bufhidden=delete ]])
     end,
 }
