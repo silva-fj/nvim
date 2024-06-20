0c9234916b99f0aceecb2e730ab22c4f2c69f138
Francisco Silva <franjs.francisco@gmail.com>
2024-06-20 09:55:20 +0200

    updating gitsigns config

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,7 +2,7 @@ return {
     "lewis6991/gitsigns.nvim",
     config = function()
         require("gitsigns").setup({
-            signs                             = {
+            signs                        = {
                 add          = { text = '┃' },
                 change       = { text = '┃' },
                 delete       = { text = '_' },
@@ -10,32 +10,29 @@ return {
                 changedelete = { text = '~' },
                 untracked    = { text = '┆' },
             },
-            signcolumn                        = true,  -- Toggle with `:Gitsigns toggle_signs`
-            numhl                             = false, -- Toggle with `:Gitsigns toggle_numhl`
-            linehl                            = false, -- Toggle with `:Gitsigns toggle_linehl`
-            word_diff                         = false, -- Toggle with `:Gitsigns toggle_word_diff`
-            watch_gitdir                      = {
+            signcolumn                   = true,       -- Toggle with `:Gitsigns toggle_signs`
+            numhl                        = false,      -- Toggle with `:Gitsigns toggle_numhl`
+            linehl                       = false,      -- Toggle with `:Gitsigns toggle_linehl`
+            word_diff                    = false,      -- Toggle with `:Gitsigns toggle_word_diff`
+            watch_gitdir                 = {
                 follow_files = true
             },
-            auto_attach                       = true,
-            attach_to_untracked               = false,
-            current_line_blame                = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-            current_line_blame_opts           = {
+            auto_attach                  = true,
+            attach_to_untracked          = false,
+            current_line_blame           = false,      -- Toggle with `:Gitsigns toggle_current_line_blame`
+            current_line_blame_opts      = {
                 virt_text = true,
                 virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
                 delay = 1000,
                 ignore_whitespace = false,
                 virt_text_priority = 100,
             },
-            current_line_blame_formatter      = '<author>, <author_time:%Y-%m-%d> - <summary>',
-            current_line_blame_formatter_opts = {
-                relative_time = false,
-            },
-            sign_priority                     = 6,
-            update_debounce                   = 100,
-            status_formatter                  = nil,   -- Use default
-            max_file_length                   = 40000, -- Disable if file is longer than this (in lines)
-            preview_config                    = {
+            current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+            sign_priority                = 6,
+            update_debounce              = 100,
+            status_formatter             = nil,        -- Use default
+            max_file_length              = 40000,      -- Disable if file is longer than this (in lines)
+            preview_config               = {
                 -- Options passed to nvim_open_win
                 border = 'single',
                 style = 'minimal',
@@ -43,7 +40,7 @@ return {
                 row = 0,
                 col = 1
             },
-            on_attach                         = function(bufnr)
+            on_attach                    = function(bufnr)
                 local gitsigns = require('gitsigns')
 
                 local function map(mode, l, r, opts)
