887932b8dee991abf2cbffeb420ca2f3dc7f6f24
Francisco Silva <francisco@kawagarbo-tech.io>
2024-10-01 07:58:34 +0000

    removing jetbrains colorscheme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,6 +1,11 @@
 return {
     {
         "ellisonleao/gruvbox.nvim",
+        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            vim.cmd.colorscheme("gruvbox")
+        end,
     },
     {
         "dracula/vim",
@@ -32,13 +37,5 @@ return {
     },
     {
         "rebelot/kanagawa.nvim",
-    },
-    {
-        'doums/darcula',
-        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
-        config = function()
-            vim.cmd.colorscheme("darcula")
-        end,
     },
 }
