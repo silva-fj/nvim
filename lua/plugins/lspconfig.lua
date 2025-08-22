f7f59fba76adee8e261015711d472bb0ed1bbc76
Francisco Silva <franjs.francisco@gmail.com>
2025-08-22 17:35:30 +0200

    refactor(lsp): update lua_ls workspace config

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -256,9 +256,6 @@ return {
         lspconfig.lua_ls.setup({
             settings = {
                 Lua = {
-                    -- diagnostics = {
-                    --     globals = { 'vim' }
-                    -- },
                     workspace = {
                         library = vim.api.nvim_get_runtime_file("", true),
                     },
